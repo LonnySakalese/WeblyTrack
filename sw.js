19ae411fb9750ef1c5834c234ee06b90a5c1dbe5
Lonny <lonnysakalesepro@gmail.com>
2025-12-26 00:39:38 +0100

    Lancement Du Tracker d'habitudes

---
+++ sw.js
@@ -0,0 +1,41 @@
+// Service Worker pour Warrior Habit Tracker
+const CACHE_NAME = 'warrior-tracker-v1';
+const urlsToCache = [
+  './',
+  './WarriorHabitTracker.html',
+  './warrior-icon.svg',
+  './manifest.json'
+];
+
+// Installation
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch avec stratégie cache-first
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activation et nettoyage des anciens caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+});
